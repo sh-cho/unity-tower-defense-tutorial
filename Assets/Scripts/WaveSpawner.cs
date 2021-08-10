19e257de14c3a58ac46497042235a9d01ddb6646
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2021-08-10 21:00:16 +0900

    Implement Spawner

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,18 +1,39 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform enemyPrefab;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float waveInterval = 5f;
+
+    private float _countdown = 2f;
+    private int _waveIndex = 0;
+
+    private void Update()
     {
-        
+        if (_countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            _countdown = waveInterval;
+        }
+
+        _countdown -= Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    // --------------------------------------------------
+    private IEnumerator SpawnWave()
     {
-        
+        ++_waveIndex;
+        for (int i = 0; i < _waveIndex; ++i)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 }
